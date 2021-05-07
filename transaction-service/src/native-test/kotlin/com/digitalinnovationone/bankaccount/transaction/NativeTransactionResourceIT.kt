9d26159ae16f508b0f27d3f8c120fe9d7faab795
Pedro Trindade <phts99@gmail.com>
2021-05-06 22:27:44 -0300

    first commit

---
+++ transaction-service/src/native-test/kotlin/com/digitalinnovationone/bankaccount/transaction/NativeTransactionResourceIT.kt
@@ -0,0 +1,6 @@
+package com.digitalinnovationone.bankaccount.transaction
+
+import io.quarkus.test.junit.NativeImageTest
+
+@NativeImageTest
+open class NativeTransactionResourceIT : TransactionResourceTest()
